9363c80abd32ea70f8cce397605178df052deba6
DanishZDN <muhammad.danish@student.umn.ac.id>
2025-02-06 12:37:17 +0700

    step 5

--- app/src/main/java/id/ac/umn/modul1_1/MainActivity.kt
+++ app/src/main/java/id/ac/umn/modul1_1/MainActivity.kt
@@ -1,4 +1,74 @@
 package id.ac.umn.modul1_1
+//
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Surface
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import id.ac.umn.modul1_1.ui.theme.Modul11Theme // Correct theme reference
+//
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            Modul11Theme {
+//                MyApp(modifier = Modifier.fillMaxSize())
+//            }
+////            Modul11Theme {
+////                // A surface container using the 'background' color from the theme
+////                Surface(
+////                    modifier = Modifier.fillMaxSize(),
+////                    color = MaterialTheme.colorScheme.background
+////                ) {
+////                    Greeting("Android")
+////                }
+////            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Surface(color = MaterialTheme.colorScheme.primary) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier.padding(24.dp)
+//        )
+//    }
+//}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview () {
+//    Modul11Theme {
+//        MyApp()
+//    }
+//}
+//
+////@Preview(showBackground = true)
+////@Composable
+////fun GreetingPreview() {
+////    Modul11Theme { // Use the correct theme in preview
+////        Greeting("Dane")
+////    }
+////}
+//
+//@Composable
+//fun MyApp(modifier: Modifier = Modifier) {
+//    Surface(
+//        modifier = modifier,
+//        color = MaterialTheme.colorScheme.background
+//    ) {
+//        Greeting("Dane2")
+//    }
+//}
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,14 +76,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import id.ac.umn.modul1_1.ui.theme.Modul11Theme // Correct theme reference
+import id.ac.umn.modul1_1.ui.theme.Modul11Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,16 +91,17 @@ class MainActivity : ComponentActivity() {
             Modul11Theme {
                 MyApp(modifier = Modifier.fillMaxSize())
             }
-//            Modul11Theme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
         }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting("Android")
     }
 }
 
@@ -44,28 +114,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         )
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview () {
+fun GreetingPreview() {
     Modul11Theme {
         MyApp()
-    }
-}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    Modul11Theme { // Use the correct theme in preview
-//        Greeting("Dane")
-//    }
-//}
-
-@Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting("Dane2")
     }
 }
